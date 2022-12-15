1901d5d6ddc01b05e8a172e4e4400ff2b758b0ac
DevDumba <marko.ivkovic@team.ai>
2022-12-15 16:51:34 +0100

    New project

---
+++ src/test/java/com/devdumba/sportcenter/SportcenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdumba.sportcenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportcenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
